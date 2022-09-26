f569f2e5df08fd92beb8bfece57227c9f0c48561
Andrew Clark <agclark2@sheffield.ac.uk>
2022-09-26 12:42:41 +0100

    Test command now places job results in a n_test directory

--- src/cosmic_ray/distribution/local.py
+++ src/cosmic_ray/distribution/local.py
@@ -13,6 +13,7 @@ To use the local distributor, set ``cosmic-ray.distributor.name = "local"`` in y
 
 import asyncio
 import logging
+import shlex
 
 from cosmic_ray.distribution.distributor import Distributor
 from cosmic_ray.mutating import mutate_and_test
@@ -28,9 +29,12 @@ class LocalDistributor(Distributor):
 
     async def _process(self, pending_work, test_command, timeout, _distributor_config, on_task_complete):
         for work_item in pending_work:
+            test_command_split = shlex.split(test_command)
+            n_tests_index = test_command_split.index('-t') + 1
+            n_tests = test_command_split[n_tests_index]
             result = await mutate_and_test(
                 mutations=work_item.mutations,
-                test_command=test_command + f" -o {work_item.job_id}.json",
+                test_command=test_command + f" -o t{n_tests}/{work_item.job_id}.json",
                 timeout=timeout,
             )
             on_task_complete(work_item.job_id, result)
